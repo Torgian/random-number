dcce7e666d909ba2cd2240da9893a146c1fdf926
LakshaJael <nihongotraveler@icloud.com>
2015-05-23 20:40:48 +0900

    added a +1

--- random.js
+++ random.js
@@ -1,4 +1,4 @@
 var userNumber = parseInt(prompt('Please choose a random number'));
 var userNumberTwo = parseInt(prompt('Now choose a second number'));
-var ranNumber = Math.floor(Math.random() * (userNumber - userNumberTwo)) + userNumberTwo;
+var ranNumber = Math.floor(Math.random() * (userNumber - userNumberTwo + 1)) + userNumberTwo;
 document.write('I rolled a number between ' + userNumber + ' and ' + userNumberTwo + ' and got a ' + ranNumber + '!');
